986a546dd09ae34bcdea08d2ea86b75ee322902b
Majey <kevinerick78@yahoo.com>
2022-05-21 15:58:47 +0300

    Commit by Kevin

--- converter.c
+++ converter.c
@@ -14,8 +14,8 @@ char *convert(unsigned long int num, int base, int lowercase)
 	char *ptr;
 
 	rep = (lowercase)
-		? "0123456789ABCDEF"
-		: "0123456789abcdef";
+		? "0123456789abcdef"
+		: "0123456789ABCDEF";
 	ptr = &buffer[49];
 	*ptr = '\0';
 	do {
